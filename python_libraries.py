db939b86935c83b53da74ba9cd4bf128d332127d
ChoiJuyoung <hajuu96123@naver.com>
2020-04-09 14:08:33 +0900

    lib

---
+++ python_libraries.py
@@ -0,0 +1,24 @@
+## 내장 함수
+## docs.python.org/ko/3/libary
+
+lst = [1, 2, 3, 4, 5]
+
+x = abs(-1) # return 1
+x = all(lst) # return True if all elements of iterable is True
+'''
+def all(iterable):
+    for element in iterable:
+        if not element:
+            return False
+    return True
+'''
+x = any(lst) # return True if any element of iterable is True
+'''
+def any(iterable):
+    for element in iterable:
+        if element:
+            return True
+    return False
+'''
+x = ascii(87)
+print(x)
