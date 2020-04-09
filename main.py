2d77905e697fb7a5f3359c657e5e8983ef497bda
ChoiJuyoung <hajuu96123@naver.com>
2020-04-09 12:11:49 +0900

    testing

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello")
+from datetime import datetime
+
+lst = [1, 2, 3, 4, 5]
+
+start = datetime.now()
+
+for _ in range(100000):
+    su = 0
+
+    for x in lst:
+        su += x
+
+end = datetime.now()
+print("SUM: " + str(su))
+print(str(end - start))
+
+start = datetime.now()
+
+for _ in range(100000):
+    su = sum(lst)
+    
+end = datetime.now()
+
+print("SUM: " + str(su))
+print(str(end - start))
