660f5a322ff30e686225d765ff14651f418180e1
ChoiJuyoung <hajuu96123@naver.com>
2020-04-22 16:53:36 +0900

    dd

--- tt.py
+++ tt.py
@@ -1,16 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_restful import Resource, Api
 
 
 app = Flask(__name__)
-api = Api(app)
 
 
-class RegistUser(Resource):
-    def post(self):
-        return {'result': 'ok'}
+@app.route('/')
+def root():
+    return {'result': 'ok'}
 
-api.add_resource(RegistUser, '/user')
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
