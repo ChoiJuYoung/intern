e112ade7ef900ade87f52b44d965d3627180c3b8
ChoiJuyoung <hajuu96123@naver.com>
2020-05-11 10:47:08 +0900

    commit

---
+++ quasar_test.py
@@ -0,0 +1,41 @@
+import urllib
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import json
+import sys
+
+def init():
+    global entire_list
+    entire_list = {}
+    #f = open("C:\\quasar.txt", 'r')
+    #entire_list = json.loads(f.read())
+
+def implement():
+    res = []
+    url = "https://quasarzone.co.kr/bbs/qb_saleinfo"
+    html = urlopen(url)
+    bs = BeautifulSoup(html, "html.parser")
+
+    '''
+    span_items = bs.find_all('span')
+    for span in span_items:
+        span.extract()
+    '''
+
+    item = bs.find_all('a', attrs={'class': 'subject-link'})
+    link = ["https://quasarznoe.co.kr" + it.attrs['href'] for it in item]
+    title = [it.text.strip() for it in item]
+
+    for li, ti in zip(link[4:], title[4:]):
+        if li in entire_list.keys():
+            if ti == entire_list[li]:
+                continue
+            res.append("제목 수정: " + entire_list[li] + " => " + ti + "\n" + li)
+            entire_list[li] = ti
+            continue
+        entire_list[li] = ti
+        res.append("신규 정보: " + ti + "\n" + li)
+    return res
+
+init()
+print(implement())
--- win32/win32_test_2.py
+++ win32/win32_test_2.py
@@ -1,14 +1,11 @@
 import win32gui
 
-hwnd1 = win32gui.FindWindow(None, "카카오톡")
-hwnd2 = win32gui.FindWindowEx(hwnd1, None, None, None)
-hwnd3 = win32gui.FindWindowEx(hwnd2, None, None, None)
-hwnd4 = win32gui.FindWindowEx(hwnd3, None, None, None)
+def find(hwnd):
+    chwnd = win32gui.FindWindowEx(hwnd, None, None, None)
+    while chwnd is not 0:
+        print(win32gui.GetClassName(chwnd))
+        find(chwnd)
+        chwnd = win32gui.FindWindowEx(hwnd, chwnd, None, None)
 
-
-total = ""
-while hwnd4 is not 0:
-    print(hwnd4)
-    total += (str(hwnd4) + " // ")
-    hwnd4 = win32gui.FindWindowEx(hwnd3, hwnd4, None, None)
-    
+hwnd1 = win32gui.FindWindow(None, "TEST - Chrome")
+find(hwnd1)
