9f418a40ad4755652d01fcb108c3d8e3ba8f716b
ChoiJuyoung <hajuu96123@naver.com>
2020-05-12 10:10:00 +0900

    quasar test

--- quasar_test.py
+++ quasar_test.py
@@ -1,35 +1,48 @@
-import urllib
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
-import sys
 
 def init():
     global entire_list
-    entire_list = {}
-    #f = open("C:\\quasar.txt", 'r')
-    #entire_list = json.loads(f.read())
+    f = open("C:\\quasar.txt", 'r')
+    entire_list = json.loads(f.read())
 
 def implement():
-    res = []
-    url = "https://quasarzone.co.kr/bbs/qb_saleinfo"
-    html = urlopen(url)
-    bs = BeautifulSoup(html, "html.parser")
+    try:
+        res = []
+        url = "https://quasarzone.co.kr/bbs/qb_saleinfo"
+        html = urlopen(url)
+        bs = BeautifulSoup(html, "html.parser")
 
-    item = bs.find_all('a', attrs={'class': 'subject-link'})
-    link = ["https://quasarznoe.co.kr" + it.attrs['href'] for it in item]
-    title = [it.text.strip() for it in item]
+        span_items = bs.find_all('span')
+        for span in span_items:
+            span.extract()
 
-    for li, ti in zip(link[4:], title[4:]):
-        if li in entire_list.keys():
-            if ti == entire_list[li]:
+        item = bs.find_all('a', attrs={'class': 'subject-link'})
+        link = ["https://quasarznoe.co.kr" + it.attrs['href'] for it in item]
+        title = [it.text.strip() for it in item]
+
+        for li, ti in zip(link[4:], title[4:]):
+            if li in entire_list.keys():
+                if ti == entire_list[li]:
+                    continue
+                res.append("제목 수정: " + entire_list[li] + " => " + ti + "\n" + li)
+                entire_list[li] = ti
+                save()
                 continue
-            res.append("제목 수정: " + entire_list[li] + " => " + ti + "\n" + li)
             entire_list[li] = ti
-            continue
-        entire_list[li] = ti
-        res.append("신규 정보: " + ti + "\n" + li)
-    return res
+            save()
+            res.append("신규 정보: " + ti + "\n" + li)
+        return res
+    except:
+        print("ERR")
+        pass
+
+def save():
+    print(entire_list)
+    f = open('C:\\quasar.txt', 'w')
+    f.write(str(entire_list).replace("\'", "\""))
 
 init()
+
 print(implement())
